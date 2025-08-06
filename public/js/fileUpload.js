472e759a14bacf3f575d7c4a6a90a9dfa7a7c4b0
Rebox <larp0319@gmail.com>
2025-08-06 16:27:54 -0700

    Finish project

--- public/js/fileUpload.js
+++ public/js/fileUpload.js
@@ -1,13 +1,33 @@
-FilePond.registerPlugin(
-  FilePondPluginImagePreview,
-  FilePondPluginImageResize,
-  FilePondPluginFileEncode
-);
+const rootStyles = window.getComputedStyle(document.documentElement);
 
-FilePond.setOptions({
-  stylePanelAspectRatio: 150 / 100,
-  imageResizeTargetWidth: 100,
-  imageResizeTargetHeight: 150,
-});
+if (
+  rootStyles.getPropertyPriority("--book-cover-width-large") != null &&
+  rootStyles.getPropertyPriority("--book-cover-width-large") != ""
+) {
+  ready();
+} else {
+  document.getElementById("main_css").addEventListener("load", ready);
+}
 
-FilePond.parse(document.body);
+function ready() {
+  const coverWidth = parseFloat(
+    rootStyles.getPropertyPriority("--book-cover-width-large")
+  );
+  const coverAspectRatio = parseFloat(
+    rootStyles.getPropertyPriority("--book-cover-aspect-ratio")
+  );
+  const coverHeight = coverWidth / coverAspectRatio;
+  FilePond.registerPlugin(
+    FilePondPluginImagePreview,
+    FilePondPluginImageResize,
+    FilePondPluginFileEncode
+  );
+
+  FilePond.setOptions({
+    stylePanelAspectRatio: 1 / coverAspectRatio,
+    imageResizeTargetWidth: coverWidth,
+    imageResizeTargetHeight: coverHeight,
+  });
+
+  FilePond.parse(document.body);
+}
